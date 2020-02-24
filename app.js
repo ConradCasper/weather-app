346f49d79869305fa392c58c654b2e155c6bb7c5
Conrad <cnrdcasper@gmail.com>
2020-02-24 12:06:44 -0500

    parsed JSON from weather api and logged it to the console

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/68db5af343b1a89c046ae07c8b1226a0/37.8267,-122.4233'
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
