02a2224e37e13b75041cdbeaec8720f6e46c7f6f
Conrad <cnrdcasper@gmail.com>
2020-02-24 13:33:24 -0500

    error handling for both requests set up.

--- app.js
+++ app.js
@@ -14,14 +14,21 @@ request({ url: url, json: true }, (error, response) => {
         const message = `${daily} It is currently ${temp} degrees out. There is a ${prob}% chance of rain.`
         log(message)
     }
-
-    
 })
 
-// const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY25yZGNhc3BlciIsImEiOiJjazcwcjRhNWwwMDBnM2RycmFubGFoeGc5In0.9eF5TIJ2O8clH5S5lUANpQ&limit=1'
+    
 
-// request({ url: url2, json: true }, (error, response) => {
-//     const lat = response.body.features[0].center[1]
-//     const long = response.body.features[0].center[0]
-//     log(lat, long)
-// })
+const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY25yZGNhc3BlciIsImEiOiJjazcwcjRhNWwwMDBnM2RycmFubGFoeGc5In0.9eF5TIJ2O8clH5S5lUANpQ&limit=1'
+
+request({ url: url2, json: true }, (error, response) => {
+    if(error){
+        log('Unable to connect to location services!')
+    } else if(response.body.features.length === 0){
+        log('Unable to find location. Try another search.')
+    } else {
+        const lat = response.body.features[0].center[1]
+        const long = response.body.features[0].center[0]
+        log(lat, long)
+    }
+})
+    
