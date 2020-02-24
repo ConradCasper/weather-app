b2e86380529b2294b008d37b431efcece598c248
Conrad <cnrdcasper@gmail.com>
2020-02-24 12:28:41 -0500

    printed out message to the console of what the current temperature is and what the chance of rain is

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const request = require('request')
-
+const log = console.log
 const url = 'https://api.darksky.net/forecast/68db5af343b1a89c046ae07c8b1226a0/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    // log(response.body.currently)
+    const temp = response.body.currently.temperature
+    const prob = response.body.currently.precipProbability
+    const message = `It is currently ${temp} degrees out. There is a ${prob}% chance of rain.`
+    log(message)
 })
