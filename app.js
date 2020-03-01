953636c2c5ee569a52a1ed9bcc08c1f6504a163a
Conrad <cnrdcasper@gmail.com>
2020-03-01 18:47:39 -0500

    app, forecast, and geocode.js to use destructuring

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const address = process.argv[2]
 if(!address){
     log('Please enter an adress.')
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location }) => {
         
         if(error){
            return log(error)
         } 
     
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error){
                 return log(error)
             }
     
-            log(data.location)
+            log(location)
             log(forecastData)
           })
     })
--- utils/forecast.js
+++ utils/forecast.js
@@ -16,15 +16,17 @@ const log = console.log
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/68db5af343b1a89c046ae07c8b1226a0/${latitude},${longitude}`
     
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error){
             callback('Unable to connect to weather service!', undefined)
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            const temp = response.body.currently.temperature
-            const prob = response.body.currently.precipProbability
-            const daily = response.body.daily.data[0].summary
+            // const temp = response.body.currently.temperature
+            // const prob = response.body.currently.precipProbability
+            const daily = body.daily.data[0].summary
+            const { temperature:temp, precipProbability:prob } = body.currently
+
             const message = `${daily} It is currently ${temp} degrees out. There is a ${prob}% chance of rain.`
             callback(undefined, message)
         }
