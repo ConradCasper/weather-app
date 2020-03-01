0877687b646baa364d2f12636dc26a7ebd4541cd
Conrad <cnrdcasper@gmail.com>
2020-03-01 14:14:28 -0500

    created forecast.js file in utils directory for forecast function challenge.

--- app.js
+++ app.js
@@ -19,25 +19,21 @@ const url = 'https://api.darksky.net/forecast/68db5af343b1a89c046ae07c8b1226a0/3
 
     
 
-// const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY25yZGNhc3BlciIsImEiOiJjazcwcjRhNWwwMDBnM2RycmFubGFoeGc5In0.9eF5TIJ2O8clH5S5lUANpQ&limit=1'
 
-// request({ url: url2, json: true }, (error, response) => {
-//     if(error){
-//         log('Unable to connect to location services!')
-//     } else if(response.body.features.length === 0){
-//         log('Unable to find location. Try another search.')
-//     } else {
-//         const lat = response.body.features[0].center[1]
-//         const long = response.body.features[0].center[0]
-//         log(lat, long)
-//     }
-// })
     
 
 
 
 
-geocode('Boston', (error, data) => {
-    log('Error', error)
-    log('Data', data)
-})
+// geocode('Boston', (error, data) => {
+//     log('Error', error)
+//     log('Data', data)
+// })
+
+
+
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
